c90f4f694c136020442ab1589e79bed3c5ee50e0
newbie181 <assirahannabawiyah8@gmail.com>
2025-05-27 16:07:51 +0700

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -1,43 +1,20 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
-#========================================================================================================================
 
-# ------------------------------- Main source started -------------------------------
-#
-# Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+# Tambah feed atau update feed jika diperlukan
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 
-# Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
-
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
-#
-# ------------------------------- Main source ends -------------------------------
-
-# ------------------------------- Other started -------------------------------
-#
-# Add luci-app-amlogic
-rm -rf package/luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-#
-# Apply patch
-# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
-#
-# ------------------------------- Other ends -------------------------------
-
-# Tambahkan package Passwall
+# Clone package Passwall
+echo "Cloning Passwall..."
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall
 
-# Tambahkan package OpenClash
+# Clone package OpenClash
+echo "Cloning OpenClash..."
 git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
 
-# Compile dan install po2lmo
+# Build dan install po2lmo (diperlukan untuk OpenClash)
+echo "Building po2lmo for OpenClash..."
 pushd package/luci-app-openclash/tools/po2lmo
-make && sudo make install
+make
+sudo make install
 popd
